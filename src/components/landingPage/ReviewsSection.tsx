9610563af37bed3e97abdc72ce8a93255355c522
Denis Junior <junior.denis2354@outlook.com>
2022-03-27 16:22:17 -0300

    simplify and remove trash

--- src/components/landingPage/ReviewsSection.tsx
+++ src/components/landingPage/ReviewsSection.tsx
@@ -8,22 +8,21 @@ type LocalProps = {};
 class ReviewsSection extends React.Component<LocalProps> {
   render() {
     return (
-      <Box sx={{ width: "100%", height: "100%" }}>
-        <Stack
-          direction="column"
-          justifyContent="center"
-          alignItems="flex-start"
-        >
-          <Typography variant="h4" component="div" fontWeight={700}>
-            Reviews by users
-          </Typography>
-          <Typography variant="body1" component="div">
-            Tell they what you think!
-          </Typography>
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="flex-start"
+        sx={{ width: "100%", height: "100%" }}
+      >
+        <Typography variant="h4" component="div" fontWeight={700}>
+          Reviews by users
+        </Typography>
+        <Typography variant="body1" component="div">
+          Tell they what you think!
+        </Typography>
 
-          <ReviewCards />
-        </Stack>
-      </Box>
+        <ReviewCards />
+      </Stack>
     );
   }
 }
--- src/components/landingPage/index.tsx
+++ src/components/landingPage/index.tsx
@@ -4,17 +4,14 @@ import { Box } from "@mui/material";
 import WelcomeSection from "./WelcomeSection";
 import ReviewsSection from "./ReviewsSection";
 import ThanksSection from "./ThanksSection";
-import ReviewRepository from "../../repositories/review/ReviewRepository";
 
 class LandingPage extends React.Component {
-  reviewRepository = new ReviewRepository();
-  
   render() {
     return (
       <>
         <Box sx={{ margin: "0 5%" }}>
           <WelcomeSection />
-          <ReviewsSection reviewRepository={this.reviewRepository} />
+          <ReviewsSection />
         </Box>
         <ThanksSection />
       </>
